9a4554194df8e0266fdfbc1b1c76615ca40e229d
The_Smashers_Design <71424660+The-Smashers-Design@users.noreply.github.com>
2020-11-22 23:21:46 +0530

    Add files via upload

---
+++ NINE.py
@@ -0,0 +1,11 @@
+#Python Program to Display the multiplication Table
+
+print("ENTER NUMBER FOR CACULATE MULTIPLICATION TABLE")
+var1=int(input())
+i=1
+while(i<=10):
+    res=int(var1)*int(i)
+    print(var1,"*",i,"=",res,"\n")
+    i=i+1
+
+    
